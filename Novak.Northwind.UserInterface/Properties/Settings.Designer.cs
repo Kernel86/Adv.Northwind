4c92f4ef22acec97317594f2c95f83b4c447a79d
Hannah Novak <shawncpu5@msn.com>
2013-04-23 02:38:56 +0000

    git-tfs-id: [https://shawnnovak.visualstudio.com/DefaultCollection]$/Northwind;C100

--- Novak.Northwind.UserInterface/Properties/Settings.Designer.cs
+++ Novak.Northwind.UserInterface/Properties/Settings.Designer.cs
@@ -22,5 +22,16 @@ namespace Novak.Northwind.UserInterface.Properties {
                 return defaultInstance;
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=vintagesylvania.net,10433;Initial Catalog=DBNovak;Persist Security In" +
+            "fo=True;User ID=CMLogin;Password=CMLogin")]
+        public string DBNovakConnectionString {
+            get {
+                return ((string)(this["DBNovakConnectionString"]));
+            }
+        }
     }
 }
---
+++ Novak.Northwind.UserInterface/frmReport2.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Novak.Northwind.UserInterface
+{
+    partial class frmReport2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.dsCustomers = new Novak.Northwind.UserInterface.dsCustomers();
+            this.CustomersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.CustomersTableAdapter = new Novak.Northwind.UserInterface.dsCustomersTableAdapters.CustomersTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dsCustomers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CustomersBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.CustomersBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "Novak.Northwind.UserInterface.rpwWizard.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(559, 351);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // dsCustomers
+            // 
+            this.dsCustomers.DataSetName = "dsCustomers";
+            this.dsCustomers.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // CustomersBindingSource
+            // 
+            this.CustomersBindingSource.DataMember = "Customers";
+            this.CustomersBindingSource.DataSource = this.dsCustomers;
+            // 
+            // CustomersTableAdapter
+            // 
+            this.CustomersTableAdapter.ClearBeforeFill = true;
+            // 
+            // frmReport2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 351);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "frmReport2";
+            this.Text = "frmReport2";
+            this.Load += new System.EventHandler(this.frmReport2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dsCustomers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CustomersBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource CustomersBindingSource;
+        private dsCustomers dsCustomers;
+        private dsCustomersTableAdapters.CustomersTableAdapter CustomersTableAdapter;
+    }
+}
