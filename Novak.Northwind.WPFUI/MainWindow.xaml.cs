b3d333b84b51c04250f261b4c274bd175778ad3c
Hannah Novak <shawncpu5@msn.com>
2013-04-25 14:34:29 +0000

    git-tfs-id: [https://shawnnovak.visualstudio.com/DefaultCollection]$/Northwind;C104

---
+++ Novak.Northwind.WPFUI/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Novak.Northwind.WPFUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShow_Click(object sender, RoutedEventArgs e)
+        {
+            List<NorthwindService.CCustomer> oCustomers = new List<NorthwindService.CCustomer>();
+
+            NorthwindService.NorthwindClient oClient = new NorthwindService.NorthwindClient;
+
+            oCustomers = oClient.GetCustomers();
+
+            grdCustomers.ItemsSource = null;
+            grdCustomers.ItemsSource = oCustomers;
+
+            oClient = null;
+        }
+    }
+}
